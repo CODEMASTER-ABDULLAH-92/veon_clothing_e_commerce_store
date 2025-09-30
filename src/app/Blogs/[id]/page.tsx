1b492f0dad40ddbe30949bf466363c607f307784
CODEMASTER-ABDULLAH-92 <abdullahpeerzada92@gmail.com>
2025-09-30 08:12:59 +0500

    BugFixing

--- src/app/Blogs/[id]/page.tsx
+++ src/app/Blogs/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      ldldl
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nulla debitis sit quisquam labore quae velit, eligendi fugit ipsa magnam doloribus eum qui obcaecati praesentium cum minus odio harum! Asperiores.
     </div>
   )
 }
